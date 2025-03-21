c250c2db939ea2421d0fd0277035fa33bb461512
Dylon Mitchell <mitchelldylon.dev@gmail.com>
2025-03-21 17:27:22 -0400

    Completed working door
    added door with triggers that open and close automatically, Lights on door to tell if its open or closed, added barrier on door after passing through so player can not go backwards while door closes.

---
+++ QaGameJamProject#1/Assets/Scripts/Dylons_Scripts/TriggerDoor.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+    private Animator _doorAnimator;
+
+    public GameObject InvisibleDoor;
+    public GameObject GreenLightSwitch;
+    public GameObject RedLightSwitch;
+
+    void Start()
+    {
+        _doorAnimator = GetComponent<Animator>();
+    }
+
+    //Open Door
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            InvisibleDoor.SetActive(false);
+            GreenLightSwitch.SetActive(true);
+            RedLightSwitch.SetActive(false);
+
+            //_doorAnimator.SetTrigger("Open");
+            _doorAnimator.SetBool("Open", true);
+            _doorAnimator.SetBool("Close", false);
+   
+        }
+    }
+
+    //Close Door
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            InvisibleDoor.SetActive(true);
+            GreenLightSwitch.SetActive(false);
+            RedLightSwitch.SetActive(true);
+
+            _doorAnimator.SetBool("Open", false);
+            _doorAnimator.SetBool("Close", true);
+        }
+    }
+
+
+}
