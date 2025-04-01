dc79267f2303323b023da9bfe6070e6eb4d5af9a
Dylon Mitchell <mitchelldylon.dev@gmail.com>
2025-03-31 21:13:13 -0400

    Added Player Sprint

--- QaGameJamProject#1/Assets/Scripts/Dylons_Scripts/PlayerMovement.cs
+++ QaGameJamProject#1/Assets/Scripts/Dylons_Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+    public float sprintSpeed;
 
     public float groundDrag;
 
@@ -29,6 +30,15 @@ public class PlayerMovement : MonoBehaviour
         myInput();
 
         rb.linearDamping = groundDrag;
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 10;
+        }
+        else
+        {
+            moveSpeed = 5;
+        }
     }
 
     private void FixedUpdate()
